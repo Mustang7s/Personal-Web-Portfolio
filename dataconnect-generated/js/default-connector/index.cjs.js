ce51b3092fb1694b570dde839cb89b33ce6d92e7
Mustang7s <102588285+Mustang7s@users.noreply.github.com>
2025-04-30 09:22:54 +0300

    update

---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'personal-web-portfolio',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
--- src/firebase-comment.js
+++ src/firebase-comment.js
@@ -1,20 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 import { collection, addDoc } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDJPq9a0YPoQYkpQ-Uaw7aXQRXzzqOKzFA",
-    authDomain: "web-kelas-tes.firebaseapp.com",
-    projectId: "web-kelas-tes",
-    storageBucket: "web-kelas-tes.appspot.com",
-    messagingSenderId: "890817433268",
-    appId: "1:890817433268:web:11e5258f8864a6174c11e1"
+  apiKey: "AIzaSyCS1407epe5nOlM27CmDA2rx-nONEqfL3M",
+  authDomain: "evans-personal-portfolio-87bdc.firebaseapp.com",
+  projectId: "evans-personal-portfolio-87bdc",
+  storageBucket: "evans-personal-portfolio-87bdc.firebasestorage.app",
+  messagingSenderId: "505508860388",
+  appId: "1:505508860388:web:d75f44b695035ba0fff7f1",
+  measurementId: "G-S51GC899SZ"
 };
 
-// Initialize with a unique name
+// Initialize Firebase with a unique name
 const app = initializeApp(firebaseConfig, 'comments-app');
 const db = getFirestore(app);
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
 
-export { db, storage, collection, addDoc };
+export { db, storage, collection, addDoc, analytics };
