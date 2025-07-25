a062a745c2518ed1b95a3b5f49a281504a4a9d22
Mustang7s <102588285+Mustang7s@users.noreply.github.com>
2025-07-25 11:44:55 +0300

    UPDATE

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'personal-web-portfolio',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'personal-web-portfolio',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
